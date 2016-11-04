4580d3793af256d7f80a3675fab83bf18f0e46d0
Ronald Ilma <rri5@cornell.edu>
2016-11-04 19:15:46 -0400

    Add files via upload

--- python/singlylinkedlist.py
+++ python/singlylinkedlist.py
@@ -25,22 +25,68 @@ class LinkedList(object):
 
         self.head = Node(head)
 
+
     def insert(self, data):
 
         new_node = Node(data)
         new_node.set_next(self.head)
         self.head = new_node
 
+
     def size(self):
 
         curr_node = self.head        
         counter = 0
-        while curr_node != None:
+        while curr_node:
             curr_node = curr_node.get_next()
             counter += 1
 
         return counter
 
+
+    def search(self, data):
+
+        curr_node = self.head
+        isFound = False
+        index = 0
+        while curr_node and not isFound:
+            if curr_node.get_data() == data:
+                isFound = True
+            else:
+                curr_node = curr_node.get_next()
+            index += 1
+
+        if not isFound:
+            print("Data %i is not found in list!" % data)
+        else:
+            print("Data %i is found in list at index %i" % (data, index))
+
+        return curr_node
+
+
+    def delete(self, data):
+
+        curr_node = self.head
+        prev_node = None
+        isFound = False
+
+        while curr_node and not isFound:
+
+            if curr_node.get_data() == data:
+                isFound = True
+            else:
+                prev_node = curr_node
+                curr_node = curr_node.get_next()
+
+        if curr_node is None:
+            print('Data %i is not found!' % data)
+            return
+
+        if prev_node is None:
+            self.head = curr_node.get_next()
+        else:
+            prev_node.set_next(curr_node.get_next())
+            
 
     def print(self):
 
@@ -70,11 +116,20 @@ if __name__ == '__main__':
     myList = LinkedList(head_value)
 
     # Add values to the linked list
-    for x in range(head_value, 0, -20):
+    for x in range(head_value - 20, 0, -20):
         myList.insert(x)
 
     # Size of linked list
     print(myList.size())
 
     # Print values of nodes 
+    myList.print()
+
+    # Search a value in linked list
+    value = 60
+    myList.search(value)
+
+    # Delete a value of a linked list
+    value = 60
+    myList.delete(value)
     myList.print()
