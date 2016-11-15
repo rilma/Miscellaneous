028eb3fded52fc781deb4bf5731b4b3ba661bf9e
Ronald Ilma <rri5@cornell.edu>
2016-11-15 17:58:43 -0500

    Create reverseinplace.py

---
+++ python/reverseinplace.py
@@ -0,0 +1,22 @@
+
+
+def ReverseInPlace(string):
+
+    """
+        Strings are inmutable so we reverse a list of characters then 
+        convert them to a string 
+    """
+
+    List = list(string)
+    nChars = len(List)
+
+    for i in range(nChars // 2):
+        List[i], List[nChars - i - 1] = List[nChars - i - 1], List[i]
+
+    return ''.join(List)
+
+
+if __name__ == '__main__':
+
+    print(ReverseInPlace('Welcome to Github'))
+
